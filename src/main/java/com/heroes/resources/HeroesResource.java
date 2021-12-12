ed2e5f5fc0688f8c91ee02b71643f909cba637c9
andrehesantos <andrehsantos28@gmail.com>
2021-12-12 13:09:02 -0300

    Inserir herói.

--- src/main/java/com/heroes/resources/HeroesResource.java
+++ src/main/java/com/heroes/resources/HeroesResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class HeroesResource {
 	@GetMapping
 	public List<Heroes> find(){
 		return (List<Heroes>) hr.findAll();
+	}
+	
+	@PostMapping("/cadastrar")
+	public Heroes addHero (@RequestBody Heroes hero) {
+		return hr.save(hero);
 	}
 }
